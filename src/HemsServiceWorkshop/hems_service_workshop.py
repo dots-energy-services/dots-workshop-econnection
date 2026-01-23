0b2bb89e3aed344894135788068b976f18aaccc7
leovsch <leo.25.1996@gmail.com>
2026-01-23 15:08:31 +0100

    initial implementation

---
+++ src/HemsServiceWorkshop/hems_service_workshop.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+import helics as h
+from dots_infrastructure.DataClasses import TimeStepInformation, EsdlId
+from dots_infrastructure.CalculationServiceHelperFunctions import get_single_param_with_name
+
+from esdl import EnergySystem
+
+from HemsServiceWorkshop.hems_service_workshop_base import HemsServiceWorkshopBase
+from HemsServiceWorkshop.hems_service_workshop_dataclasses import OptimizeConsumptionOutput
+
+class HemsServiceWorkshop(HemsServiceWorkshopBase): 
+
+    def init_calculation_service(self, energy_system: EnergySystem):
+        super().init_calculation_service(energy_system)
+
+    def optimize_consumption(self, param_dict : dict, simulation_time : datetime, time_step_number : TimeStepInformation, esdl_id : EsdlId, energy_system : EnergySystem):
+        current_reactive_power  = get_single_param_with_name(param_dict, "current_reactive_power")
+        current_active_power  = get_single_param_with_name(param_dict, "current_active_power")
+        pv_active_power  = get_single_param_with_name(param_dict, "pv_active_power")
+        max_charge_active_power  = get_single_param_with_name(param_dict, "max_charge_active_power")
+        max_discharge_active_power  = get_single_param_with_name(param_dict, "max_discharge_active_power")
+
+        active_power_to_charge = 0
+        aggregated_active_power_1phase = current_active_power / 3
+        aggregated_reactive_power_1phase = current_reactive_power / 3
+        aggregated_active_power = [aggregated_active_power_1phase, aggregated_active_power_1phase, aggregated_active_power_1phase]
+        aggregated_reactive_power = [aggregated_reactive_power_1phase, aggregated_reactive_power_1phase, aggregated_reactive_power_1phase]
+        active_power_to_charge = 0
+        if 0 < pv_active_power - current_active_power < max_charge_active_power:
+            active_power_to_charge = pv_active_power - current_active_power
+            aggregated_active_power = [0,0,0]
+
+        return OptimizeConsumptionOutput(aggregated_active_power, aggregated_reactive_power, active_power_to_charge)
+
+
+if __name__ == "__main__":
+
+    helics_simulation_executor = HemsServiceWorkshop()
+    helics_simulation_executor.start_simulation()
+    helics_simulation_executor.stop_simulation()
