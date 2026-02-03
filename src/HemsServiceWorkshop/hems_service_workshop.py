0da1c2d71fd3d5be030bbed60fd464be7f04c124
Joep <j.v.schelven@student.tue.nl>
2026-02-03 16:44:21 +0100

    test

--- src/HemsServiceWorkshop/hems_service_workshop.py
+++ src/HemsServiceWorkshop/hems_service_workshop.py
@@ -28,9 +28,16 @@ class HemsServiceWorkshop(HemsServiceWorkshopBase):
         aggregated_active_power = [aggregated_active_power_1phase, aggregated_active_power_1phase, aggregated_active_power_1phase]
         aggregated_reactive_power = [aggregated_reactive_power_1phase, aggregated_reactive_power_1phase, aggregated_reactive_power_1phase]
         active_power_to_charge = 0
-        # if 0 < pv_active_power - current_active_power < max_charge_active_power:
-        #     active_power_to_charge = 0 pv_active_power - current_active_power
-        #     aggregated_active_power = [0,0,0]
+
+        residual_generation = pv_active_power - current_active_power
+        if 0 < residual_generation < max_charge_active_power:
+            active_power_to_charge = pv_active_power - current_active_power
+            aggregated_active_power = [0,0,0]
+        elif residual_generation < 0
+            active_power_to_charge = pv_active_power - current_active_power
+
+
+
 
         return OptimizeConsumptionOutput(aggregated_active_power, aggregated_reactive_power, active_power_to_charge)
 
