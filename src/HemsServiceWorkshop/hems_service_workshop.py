6e038b84277494f6077d3f42e51bc7536952fae5
martvangassel <martvangassel@gmail.com>
2026-02-10 13:26:04 +0100

    updated battery code

--- src/HemsServiceWorkshop/hems_service_workshop.py
+++ src/HemsServiceWorkshop/hems_service_workshop.py
@@ -21,6 +21,9 @@ class HemsServiceWorkshop(HemsServiceWorkshopBase):
             pv_active_power = 0
         max_charge_active_power  = get_single_param_with_name(param_dict, "max_charge_active_power")
         max_discharge_active_power  = get_single_param_with_name(param_dict, "max_discharge_active_power")
+        market_price = get_single_param_with_name(param_dict, "market_price")
+        if market_price == None:
+            market_price = 0
 
         active_power_to_charge = 0
         aggregated_active_power_1phase = current_active_power / 3
@@ -29,12 +32,16 @@ class HemsServiceWorkshop(HemsServiceWorkshopBase):
         aggregated_reactive_power = [aggregated_reactive_power_1phase, aggregated_reactive_power_1phase, aggregated_reactive_power_1phase]
         active_power_to_charge = 0
         
+        # If market price is above 0.1, maximize discharge
+        if market_price > 0.1:
+            active_power_to_charge = max_discharge_active_power
+            aggregated_active_power = [0,0,0]
         # If there is a deficit in power, discharge the battery to cover it
-        if 0 > pv_active_power - current_active_power > max_discharge_active_power:
+        elif 0 > pv_active_power - current_active_power > max_discharge_active_power:
             active_power_to_charge = pv_active_power - current_active_power
             aggregated_active_power = [0,0,0]
         # If there is surplus PV power, use it to charge the battery
-        if 0 < pv_active_power - current_active_power < max_charge_active_power:
+        elif 0 < pv_active_power - current_active_power < max_charge_active_power:
             active_power_to_charge = pv_active_power - current_active_power
             aggregated_active_power = [0,0,0]
 
