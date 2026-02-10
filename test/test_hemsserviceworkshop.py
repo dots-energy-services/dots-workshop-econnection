bd7e5dbdf0206a883ea67f97d2a0bc0df737626b
20173989 <j.j.markus@tue.nl>
2026-02-10 13:53:28 +0100

    Market Price Publishing and Control with codegen and test cases

--- test/test_hemsserviceworkshop.py
+++ test/test_hemsserviceworkshop.py
@@ -31,6 +31,7 @@ class Test(unittest.TestCase):
             "pv_active_power": 110,
             "max_charge_active_power": 8,
             "max_discharge_active_power": -8,
+            "day_ahead_price":1,
         }
 
         hems_service = HemsServiceWorkshop()
@@ -46,6 +47,7 @@ class Test(unittest.TestCase):
             "pv_active_power": 108,
             "max_charge_active_power": 8,
             "max_discharge_active_power": -8,
+            "day_ahead_price": 1,
         }
 
         hems_service = HemsServiceWorkshop()
