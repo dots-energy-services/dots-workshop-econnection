70562b78584b1d12d0fa3a6fd4df193c248e0100
andrei <a.dobre1@student.tue.nl>
2026-02-11 11:15:45 +0100

    test

--- src/HemsServiceWorkshop/hems_service_workshop.py
+++ src/HemsServiceWorkshop/hems_service_workshop.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import helics as h
-from dots_infrastructure.DataClasses import TimeStepInformation, EsdlId, HelicsCalculationInformation
+from dots_infrastructure.DataClasses import TimeStepInformation, EsdlId, HelicsCalculationInformation, get_single_param_with_name
 from dots_infrastructure.HelicsFederateHelpers import HelicsSimulationExecutor
 from esdl import EnergySystem
 
@@ -10,7 +10,14 @@ class HemsServiceWorkshop(HemsServiceWorkshopBase):
         super().init_calculation_service(energy_system)
 
     def optimize_consumption(self, param_dict : dict, simulation_time : datetime, time_step_number : TimeStepInformation, esdl_id : EsdlId, energy_system : EnergySystem):
-        pass
+        current_reactive_power  = get_single_param_with_name(param_dict, "current_reactive_power")
+        current_active_power  = get_single_param_with_name(param_dict, "current_active_power")
+        aggregated_active_power = current_active_power*2
+        aggregated_rective_power = current_reactive_power*2
+        return {    
+            "active_power_to_charge" : aggregated_active_power,
+            "reactive_power_to_charge" : aggregated_rective_power
+        }
 
 if __name__ == "__main__":
 
